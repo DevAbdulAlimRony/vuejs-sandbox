8f9a110b8220eeaa320b0aaec2e72d2ea0034688
Md. Abdul Alim <92217313+AbdulAlimRony@users.noreply.github.com>
2023-07-29 15:23:08 +0600

    methods and events

--- main.js
+++ main.js
@@ -5,8 +5,28 @@ const app = Vue.createApp({
     // data: function() {} or, data(){} - data always take function that returns as key-value pair
     data(){
         return{
-            courseGoal: "Finish VueJs"
-        };
+            courseGoal: "Finish VueJs Quickly",
+            courseGoalB: "<h2>Use of v-html directive</h2>",
+            courseLink: "https://www.udemy.com/course/vuejs-2-the-complete-guide/",
+            goal: 0,
+           };
+    },
+    //Method takes function which do actions or events
+    methods: {
+        //Method Name/Key: function(){} or shortcut
+            courseGoalMethod(){
+                const num = Math.random();
+                if(num < 0.5){
+                    //this keyword in vue take all from data object and merge it with createApp() instance
+                    return this.courseGoal;
+                }
+                else{
+                    return "Walk Slowly through this course";
+                }
+            },
+            reduceGoal(num){
+                this.goal = this.goal - num;
+            },
     }
 });
 //mount() connects vue with html
