c7b97e12a44153d92bbe3aa13e969b72a997164b
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2025-12-13 14:46:52 +0600

    basics

--- 01-App.js
+++ 01-App.js
@@ -13,70 +13,78 @@ import { createApp } from 'vue';
 // const app = createApp(App)
 
 const app = Vue.createApp({
-    
-/*
-  \-> data is a property that can be used in html which part is connected here
-  /-> data: function() {} or, data(){} - data always take function that returns as key-value pair
-*/
-    data(){
-        return{
+
+    /*
+      \-> data is a property that can be used in html which part is connected here
+      /-> data: function() {} or, data(){} - data always take function that returns as key-value pair
+    */
+    data() {
+        return {
             yourName: "Abdul Alim",
             courseGoal: "Finish VueJs Quickly",
             courseGoalB: "<h2>Use of v-html directive</h2>",
             courseLink: "https://www.udemy.com/course/vuejs-2-the-complete-guide/",
             goal: 0,
-           };
+        };
     },
-   
-/*
-  \-> Method takes function which do actions or events
-  /-> Method Name/Key: function(){} or shortcut
-*/
+
+    // The object we are passing into createApp is in fact a component. Every app requires a "root component" that can contain other components as its children.
+    // Exmp: App.vue can be our root componet. const app = createApp(App)
+    // An application instance won't render anything until its .mount() method is called.
+    // app.mount('#app'), <div id="app"></div>
+    // Defining an app-level error handler that captures errors from all descendant components: app.config.errorHandler = (err) => {}
+    // The createApp API allows multiple Vue applications to co-exist on the same page, each with its own scope for configuration and global assets like User and Admin.
+    // If you are using Vue to enhance server-rendered HTML and only need Vue to control specific parts of a large page, avoid mounting a single Vue application instance on the entire page. 
+
+    /*
+      \-> Method takes function which do actions or events
+      /-> Method Name/Key: function(){} or shortcut
+    */
     methods: {
-            setName(event){
-                this.yourName =  event.target.value;
-            },
-            reduceGoal(num){
-                this.goal = this.goal - num;
-            },
-            submitForm(){
-                alert("Form Action taken without page refresh");
-            },
-            resetInput(){
-                this.yourName = 'Abdul Alim';
-            }
-    }, 
-    
-/*
-  \-> Preventing re-execution for all methods, just execute method which is called
-  /-> don't call courseGoalMethod() in html, just write courseGoalMethod to point the method
-  \-> don't bind events in computed property, Bind in Method 
-  /-> this keyword in vue take all from data object and merge it with createApp() instance
-*/
+        setName(event) {
+            this.yourName = event.target.value;
+        },
+        reduceGoal(num) {
+            this.goal = this.goal - num;
+        },
+        submitForm() {
+            alert("Form Action taken without page refresh");
+        },
+        resetInput() {
+            this.yourName = 'Abdul Alim';
+        }
+    },
+
+    /*
+      \-> Preventing re-execution for all methods, just execute method which is called
+      /-> don't call courseGoalMethod() in html, just write courseGoalMethod to point the method
+      \-> don't bind events in computed property, Bind in Method 
+      /-> this keyword in vue take all from data object and merge it with createApp() instance
+    */
     computed: {
-        courseGoalMethod(){
+        courseGoalMethod() {
             const num = Math.random();
-            if(num < 0.5){
+            if (num < 0.5) {
                 return this.courseGoal;
             }
-            else{
+            else {
                 return "Walk Slowly through this course";
             }
         }
     },
 
-/*
-  \-> watcher: can be used as the alternative of computed, but that is worse
-  /-> If we wanna change any data or computed property if something happens, then use it
-  \-> name(old value, new value)= old value will automatically point to this.name
-  /-> goal(value) = goal is from data (must be same name) and value(old value) means this.goal
-  /-> Use Case: Send http request when certain data changes
-*/
+    /*
+      \-> watcher: can be used as the alternative of computed, but that is worse
+      /-> If we wanna change any data or computed property if something happens, then use it
+      \-> name(old value, new value)= old value will automatically point to this.name
+      /-> goal(value) = goal is from data (must be same name) and value(old value) means this.goal
+      /-> Use Case: Send http request when certain data changes
+    */
 
-//"side effects" in reaction to state changes - for example, mutating the DOM, or changing another piece of state based on the result of an async operation.
+    //"side effects" in reaction to state changes - for example, mutating the DOM, or changing another piece of state based on the result of an async operation.
     watch: {
-        goal(value){
-            if(value < 0){
+        goal(value) {
+            if (value < 0) {
                 this.goal = 0;
             }
         },
