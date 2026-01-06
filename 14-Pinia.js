4f962f02fefe57f662af4d966bef42b2942c5794
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2026-01-06 12:56:13 +0600

    pinia

--- 14-Pinia.js
+++ 14-Pinia.js
@@ -1,3 +1,31 @@
-//No mutations in pinia unlike vuex, modular by design, built in typescript support.
-//It has three concepts, the state, getters and actions and it's safe to assume these concepts are the equivalent of data, computed and methods in components.
-//See Pinia Task Management Project and documentation.
+// Pinia is a store library for Vue, it allows you to share a state across components/pages.
+// Though we can share state using composables,  but exposes your application to security vulnerabilities if it is server side rendered.
+// Even in small single page applications, We get a lot from using Pinia
+// Compared to Vuex, Pinia provides a simpler API with less ceremony, offers Composition-API-style APIs, and most importantly, has solid type inference support when used with TypeScript.
+// Compared to Vuex, Mutations no longer exist. No more magic strings to inject, import the functions, call them, enjoy autocompletion. No need to dynamically add stores, they are all dynamic by default. Pinia offers a flat structuring by design while still enabling ways of cross composition among stores. No namespaced modules.
+// Install: npm install pinia
+
+// Registering in app.js
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.mount('#app')
+
+// store.js
+import { defineStore } from 'pinia'
+export const useCounterStore = defineStore('counter', {
+    state: () => {
+        return { count: 0 }
+    },
+    // or, state: () => ({ count: 0 })
+    actions: {
+        increment() {
+            this.count++
+        },
+    },
+})
