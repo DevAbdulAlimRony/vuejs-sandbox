8d7382b0f2e2a9b5e0066e0355f6b227f63477c3
DevAbdulAlimRony <alim15@cse.pstu.ac.bd>
2026-01-14 16:01:38 +0600

    vite import and plugins

---
+++ 38- Vite.js
@@ -0,0 +1,100 @@
+// Vite is a build tool to provide afaster and leaner development experince.
+// Two major parts: A dev server, a build command that bundles your code.
+// Vite injects some runtime code to make the development server work.
+// Vite pre-bundles dependencies using esbuild. esbuild is written in Go and pre-bundles dependencies 10-100x faster than JavaScript-based bundlers.
+
+// Project: npm create vite@latest
+// Manual Install: npm install -D vite
+// During development Vite is a server, and index.html is the entry point to your application.
+// Defining command line interface in package.jso:
+// {
+//     "scripts": {
+//         "dev": "vite", // start dev server, aliases: `vite dev`, `vite serve`
+//         "build": "vite build", // build for production
+//         "preview": "vite preview" // locally preview production build
+//   There are other cli options like ssr, port, host etc.
+//     }
+// }
+
+// Features:
+// npm dependency resolving and pre-bundling.
+// Hot Module Replacement
+// Typescript support and Compiler options.
+// Simple to build single-page and multi-page applications.
+// All modern frameworks maintain integrations with Vite.
+// .jsx and .tsx files are also supported out of the box.
+// Importing .css files will inject its content to the page via a <style> tag with HMR support.
+// If the project contains valid PostCSS config (any format supported by postcss-load-config, e.g. postcss.config.js), it will be automatically applied to all imported CSS.
+// Any CSS file ending with .module.css is considered a CSS modules file.
+// Vite targets modern browsers only, it is recommended to use native CSS variables with PostCSS plugins that implement CSSWG drafts (e.g. postcss-nesting) and author plain, future-standards-compliant CSS.
+// If using Vue single file components, this also automatically enables <style lang="sass"> et al.
+// Vite improves @import resolving for Sass and Less so that Vite aliases are also respected.
+// Can also use CSS modules combined with pre-processors by prepending .module to the file extension, for example style.module.scss.
+// Insteda of postcss, we can use lightning css also.
+// Vite automatically extracts the CSS used by modules in an async chunk and generates a separate file for it.
+//  The CSS file is automatically loaded via a <link> tag when the associated async chunk is loaded,
+// When you install a modern Laravel project (especially using Laravel Vite, which is the current standard), PostCSS support is included automatically.
+// npx tailwindcss init -p, which creates the PostCSS configuration for tailwind.
+
+// If we don't want any css to inject into a page: Use ?inline
+import otherStyles from './bar.css?inline'
+
+// Static Assets:
+import imgUrl from './img.png'
+
+// Importing json files:
+import json from './example.json' // Entire object
+import { field } from './example.json' // import a root field as named exports - helps with tree-shaking!
+
+// Importing multiple modules:
+const modules = import.meta.glob('./dir/*.js')
+// Matched files are by default lazy-loaded via dynamic import and will be split into separate chunks during build.
+// Can pass eager if imported all modules directly:
+const modules2 = import.meta.glob('./dir/*.js', { eager: true })
+const modules3 = import.meta.glob(['./dir/*.js', './another/*.js'])
+
+// Named Imports:
+const modules4 = import.meta.glob('./dir/*.js', { import: 'setup', eager: true });
+// set  import : 'default': It will import the default export
+
+// Also can import custom query by option:  query : '?raw',
+// Can give base: options to use base path.
+
+// Dynamic Import:
+const module5 = await import(`./dir/${file}.js`)
+
+// Importing Web Worker:
+const worker = new Worker(new URL('./worker.js', import.meta.url))
+
+// Adding a Plugin: To use a plugin:
+// needs to be added to the devDependencies of the project. Exmp: laravel-vite-plugin, @vitejs/plugin-vue for a laravel vue application.
+// Include in the plugins array in the vite.config.js file.
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                "resources/css/preloader.css",
+                "resources/js/admin/js/app.js",
+                "resources/js/user/js/app.js"
+            ],
+            refresh: true,
+
+            // If need pre build: enforce: 'pre'
+            // If only use during build or serve: apply: 'build'
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
+
+// Vite caches the pre-bundled dependencies in node_modules/.vite
+// Resolved dependency requests are strongly cached with HTTP headers max-age=31536000,immutable to improve page reload performance during dev. 
